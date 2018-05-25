b7bbc7676adcb8684aefb3006dc6303a37ae1a2b
Estudiantes <Estudiantes@10.20.151.59>
2018-05-25 12:09:13 -0500

    commi 2

--- src/principal/Calculador.java
+++ src/principal/Calculador.java
@@ -2,8 +2,23 @@
 package principal;
 
 
-public class Calculador implements Punto {
+public class Calculador implements ICalculador {
     
     public double punto1;
     public double punto2;
+
+    @Override
+    public void calcularDistancia() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void clacularArea() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void calcularPendiente() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
